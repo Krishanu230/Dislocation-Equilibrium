f334d83dd3994b919517329421753e4c803935ca
krishanu <krishanu230@hotmail.com>
2020-11-20 21:18:57 +0530

    dislocation movement

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ def overallDisForcesXY(disArr,  materialParams):
         #we will calculate force on dis due to all other dislocations
         dis = disArr[i]
         modB = np.sqrt((dis.BurgerVec).dot(dis.BurgerVec))
+        sign = dis.Sign
         cumulativeForce = 0
         xPos = dis.Position[0]
         yPos = dis.Position[1]
@@ -70,7 +71,8 @@ def overallDisForcesXY(disArr,  materialParams):
                 continue
             else:
                 #else add the force due to dis2 on original dis
-                cumulativeForce += modB*sigmaXY(dis2, xPos, yPos, materialParams)
+                #print("stress on dis ", i, " from other is ", sigmaXY(dis2, xPos, yPos, materialParams))
+                cumulativeForce += sign*modB*sigmaXY(dis2, xPos, yPos, materialParams)
         forceArr[i] = cumulativeForce
     #now we have filled up the array containing forec on all dislocations due to all other dislocations
     return forceArr
@@ -83,7 +85,6 @@ def randomDislocationArr(numDislocation, xLimit, yLimit):
     #we also maintain a set of already selected random points just so we dont accidentaly choose the same point again
     alreadySelectedPoints = set()
 
-    count = 0
     #generate n random dislocaions
     while(len(alreadySelectedPoints)!= numDislocation):
         randomXposI = rng.integers(int(xLimit/(1e-6)))
@@ -98,7 +99,11 @@ def randomDislocationArr(numDislocation, xLimit, yLimit):
         if (xPos, yPos) not in alreadySelectedPoints:
             alreadySelectedPoints.add((xPos, yPos))
             b1 = np.array([-2e-10, 0, 0])
-            sign = +1
+            #assign half negative half positive signs
+            if (len(alreadySelectedPoints))%2==0:
+                sign = +1
+            else:
+                sign = -1
             p = np.array([xPos*1e-6, yPos*1e-6, 0])
             dis = Dislocation(b1, sign, p)
             disArr.append(dis)
@@ -107,25 +112,44 @@ def randomDislocationArr(numDislocation, xLimit, yLimit):
     return disArr
 
 def printDislocationArr(darr):
-    print('{:15} {:6}  {:6}'.format('', "X-Cordinate", "Y - Cordinate"))
+    print('{:15} {:6}  {:6}   {:6}'.format('', "X-Cordinate", "Y - Cordinate", "Sign"))
     for i in range(len(darr)):
-        print('{:15} {:11.4}  {:11.4}'.format('Dislocation '+str(i), darr[i].Position[0], darr[i].Position[1]))
+        print('{:15} {:11.4}  {:11.4}  {:11}'.format('Dislocation '+str(i), darr[i].Position[0], darr[i].Position[1], darr[i].Sign))
 
+def plotStress(darr, xx, yy, stress,fileName):
+    fig = plt.figure()  # 10 inch by 10 inch
+    #main plot
+    plt.pcolormesh(xx, yy, stress, vmin=-1e7, vmax=1e7, cmap='Spectral_r', shading='gouraud')
+    #include the side colorbar
+    plt.colorbar()
+    #add the edge Dislocation sign at the position of dislocation
+    Tsign = ((-7, 0), (0, 0), (0, -8), (0, 0), (7, 0))
+    Trevsign= ((-7, 0), (0, 0), (0, 8), (0, 0), (7, 0))
+    for dis in darr:
+        if dis.Sign == +1:
+            plt.plot(dis.Position[0],dis.Position[1],marker=Trevsign,markeredgecolor='white',markersize=10,markeredgewidth=2)
+        if dis.Sign == -1:
+            plt.plot(dis.Position[0],dis.Position[1],marker=Tsign,markeredgecolor='white',markersize=10,markeredgewidth=2)
+    #include grids in  the figure
+    plt.grid()
+    plt.savefig(fileName)
+    plt.close(fig)
 #initialise material constants
 materialParams = MaterialParameters(0.3, 30e9)  # (nu, G)
 #make burger vec, line vec, and the position of dislocation
-b1 = np.array([-2e-10, 0, 0])
+#b1 = np.array([-2e-10, 0, 0])
 #l1 = np.array([0, 0, -1])
-sign = +1
-p1 = np.array([1e-6, 1e-6, 0])
-p2 = np.array([1e-6, 1.25e-6, 0])
-xLimit = 4e-6
-yLimit = 4e-6
+#sign = +1
+#p1 = np.array([1e-6, 1e-6, 0])
+#p2 = np.array([1e-6, 1.25e-6, 0])
+xLimit = 3e-6
+yLimit = 3e-6
 #create a dislocation using above parameters
-d1 = Dislocation(b1, sign, p1)
-d2 = Dislocation(b1, sign, p2)
-
-darr = randomDislocationArr(3, xLimit, yLimit)
+#d1 = Dislocation(b1, sign, p1)
+#d2 = Dislocation(b1, sign, p2)
+numDis = int(input("No of Dilocations?"))
+darr = randomDislocationArr(numDis, xLimit, yLimit)
+darrinit = darr
 printDislocationArr(darr)
 forcearr = overallDisForcesXY(darr, materialParams)
 print("Force on dislocaions")
@@ -138,35 +162,40 @@ xx, yy = np.meshgrid(np.linspace(0, xLimit, 100), np.linspace(0, yLimit,120))
 stressXY = overallStress(darr, materialParams, xx, yy)
 #print(stressXY)
 
-#plot it
-plt.figure()  # 10 inch by 10 inch
-#main plot
-plt.pcolormesh(xx, yy, stressXY, vmin=-1e7, vmax=1e7, cmap='Spectral_r', shading='gouraud')
-#include the side colorbar
-plt.colorbar()
+plotStress(darr,xx,yy, stressXY,"start")
 
-#add the edge Dislocation sign at the position of dislocation
-Tsign = ((-7, 0), (0, 0), (0, -8), (0, 0), (7, 0))
-Trevsign= ((-7, 0), (0, 0), (0, 8), (0, 0), (7, 0))
-for dis in darr:
-    plt.plot(dis.Position[0],dis.Position[1],marker=Trevsign,markeredgecolor='white',markersize=10,markeredgewidth=2)
-#include grids in  the figure
-plt.grid()
-plt.savefig("xystart")
-
-maxTimeSteps = input("MaxTimeSteps")
-maxDelX = np.float64(input("Maximum delta x? in e-6"))*1e-6
-
+maxTimeSteps = int(input("MaxTimeSteps"))
+maxDelX = ((np.float64(input("Maximum delta x? in terms of percentage of width")))/100)*xLimit
+print(maxDelX)
 for itr in range(maxTimeSteps):
-    ForceXY = overallDisForcesXY(darr, materialParams, xx, yy)
-    maxForce = np.max(stressXY)
+    print('{:15} {}  {:4}'.format('', "Iteration", itr))
+    forceXY = overallDisForcesXY(darr, materialParams)
+    print("Force on dislocations")
+    print(forceXY)
+    maxForce = np.max(abs(forceXY))
     delT = maxDelX/maxForce
-
     #now we move each dislocation
-    for dis in darr:
+    for i in range(len(darr)):
+        dis = darr[i]
         #no change in y cordinate as no climb consideration assuming no diffusion at room temp
-        dis.Position[0] = dis.Position[0] + delT*ForceXY
+        delt = dis.Position[0] + (delT*forceXY[i])
+        #print(delt)
+        dis.Position[0] += delt
         if dis.Position[0] >xLimit:
-            dis.Position[0] =  dis.Position[0] - xLimit
+            dis.Position[0] =  dis.Position[0] - xLimit*np.round(dis.Position[0]/xLimit)
         if dis.Position[0] <0:
-            dis.Posiotion[0] = dis.Position[0] + xLimit
+            dis.Position[0] = dis.Position[0] + xLimit*np.round(abs(dis.Position[0]/xLimit)) + xLimit
+    printDislocationArr(darr)
+    #plot the iteration
+    stressXY = overallStress(darr, materialParams, xx, yy)
+    filename = "xy"+str(itr)
+    plotStress(darr,xx,yy, stressXY,filename)
+
+printDislocationArr(darr)
+plotStress(darr,xx,yy, stressXY,"final")
+maxDisp = np.float64(0)
+for i in range(len(darr)):
+    disp = darrinit[i].Position[0] - darr[i].Position[0]
+    disp = disp - xLimit*np.round(disp/xLimit)
+    if abs(disp) > maxDisp:
+        maxDisp = disp
